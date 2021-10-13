b4080cf8e5f842adc40dd04990fa86d9a9ae6d4d
mauricio-molina-git <81722575+mauricio-molina-git@users.noreply.github.com>
2021-10-13 16:39:54 -0300

    Desarrollo pantalla conf horario
     conf horario desarrollada

---
+++ app/src/main/java/com/example/superandome_appfinal/configurarHorario.java
@@ -0,0 +1,14 @@
+package com.example.superandome_appfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class configurarHorario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configurar_horario);
+    }
+}
