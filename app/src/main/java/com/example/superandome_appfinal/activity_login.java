7e91fd3cc7dde0cafba73c6ca4c56938b1e3c7dc
JRodriguez-1998 <jose.rodriguez@alumnos.frgp.utn.edu.ar>
2021-10-15 20:12:34 -0300

    Pantalla Alta Usuario

--- app/src/main/java/com/example/superandome_appfinal/activity_login.java
+++ app/src/main/java/com/example/superandome_appfinal/activity_login.java
@@ -2,7 +2,9 @@ package com.example.superandome_appfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_login extends AppCompatActivity {
 
@@ -12,5 +14,11 @@ public class activity_login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         getSupportActionBar().hide();
+    }
+
+    //Clic en Crear Cuenta
+    public void ir_crearCuenta(View view){
+        Intent i= new Intent(this,activity_altaConsultante.class);
+        startActivity(i);
     }
 }
