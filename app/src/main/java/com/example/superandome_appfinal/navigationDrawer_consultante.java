05055f500f1fda8709f9e0525eb4db76ae716c73
mauricio-molina-git <81722575+mauricio-molina-git@users.noreply.github.com>
2021-10-18 18:33:39 -0300

    Sugerir consejo diario
    Desarrollo pantalla para sugerir consejo diario.

--- app/src/main/java/com/example/superandome_appfinal/navigationDrawer_consultante.java
+++ app/src/main/java/com/example/superandome_appfinal/navigationDrawer_consultante.java
@@ -38,7 +38,8 @@ public class navigationDrawer_consultante extends AppCompatActivity {
         });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_cargarEmocionDiaria, R.id.nav_configurarHorario, R.id.nav_sugerirContenido,R.id.nav_ingresarEncuesta,R.id.nav_rutinaDiaria)
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_cargarEmocionDiaria, R.id.nav_configurarHorario, R.id.nav_sugerirContenido, R.id.nav_sugerirConsejo,
+                R.id.nav_ingresarEncuesta,R.id.nav_rutinaDiaria)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation_drawer_consultante);
