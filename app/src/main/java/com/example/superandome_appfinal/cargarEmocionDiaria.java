ba0369cd381c38d7fb3261acfe7a24d9357e78fc
mauricio-molina-git <81722575+mauricio-molina-git@users.noreply.github.com>
2021-10-19 16:37:03 -0300

    Profesional: cargar contenido multimedia
    desarrollo de pantalla cargar contenido multimedia por parte del profesional

--- app/src/main/java/com/example/superandome_appfinal/cargarEmocionDiaria.java
+++ app/src/main/java/com/example/superandome_appfinal/cargarEmocionDiaria.java
@@ -10,34 +10,6 @@ import android.view.ViewGroup;
 
 public class cargarEmocionDiaria extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    public cargarEmocionDiaria() {
-        // Required empty public constructor
-    }
-
-    public static cargarEmocionDiaria newInstance(String param1, String param2) {
-        cargarEmocionDiaria fragment = new cargarEmocionDiaria();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
