9752777cf0959e4c10cc28415a2b8437b777a435
JRodriguez-1998 <jose.rodriguez@alumnos.frgp.utn.edu.ar>
2021-10-22 11:45:22 -0300

    Actualización Login y Alta Consultante
    En Alta Consultante, se agregó la posibilidad de contestar una pregunta de Seguridad para el caso en que el Usuario haya olvidado su contraseña.
    En Login, se agregó el enlace para poder redirigirse a restablecer su contraseña.

--- app/src/main/java/com/example/superandome_appfinal/activity_altaConsultante.java
+++ app/src/main/java/com/example/superandome_appfinal/activity_altaConsultante.java
@@ -8,7 +8,7 @@ import android.widget.Spinner;
 
 public class activity_altaConsultante extends AppCompatActivity {
 
-    Spinner spinnerGenero;
+    Spinner spinnerGenero, spinnerPreguntas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +18,20 @@ public class activity_altaConsultante extends AppCompatActivity {
         getSupportActionBar().hide();
 
         spinnerGenero = (Spinner) findViewById(R.id.spinnerGeneroConsultante);
+        spinnerPreguntas = (Spinner) findViewById(R.id.spinnerPreguntas);
 
-        String [] generos = {"Masculino","Femenino","Otros"};
+        String [] generos = {"Seleccione Género","Masculino","Femenino","Otros"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_generos, generos);
         spinnerGenero.setAdapter(adapter);
+
+        String [] preguntas = {
+                                "Seleccione Pregunta de Seguridad",
+                                "¿Cómo se llamaba tu primera mascota?",
+                                "¿Cúal fue la primera película que viste en el cine?",
+                                "¿Cuál es tu equipo de Fútbol favorito?",
+                                "¿En qué Ciudad naciste?",
+                                "¿En qué fecha descendió RiBer Plate?"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, R.layout.spinner_generos, preguntas);
+        spinnerPreguntas.setAdapter(adapter2);
     }
 }
