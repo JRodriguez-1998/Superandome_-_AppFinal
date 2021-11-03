394d84a9c582e079169b12e8de0c4d7153b040a0
mauricio-molina-git <81722575+mauricio-molina-git@users.noreply.github.com>
2021-11-03 16:46:08 -0300

    Desarrollo diagologo/panel
    Buenas, desarrolle el panel. El mismo se usa de ejemplo en la pantalla de registrar emoción diaria

---
+++ app/src/main/java/com/example/superandome_appfinal/Dialogos/dialogo.java
@@ -0,0 +1,57 @@
+package com.example.superandome_appfinal.Dialogos;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.superandome_appfinal.R;
+
+public class dialogo extends DialogFragment {
+
+    Activity actividad;
+    TextView txtEstatus, txtMensaje, btnAceptar;
+
+    public dialogo() {}
+
+    @NonNull
+    @Override
+    public AlertDialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return crearDialogo();
+    }
+
+    private AlertDialog crearDialogo(){
+        AlertDialog.Builder builder  = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View v = inflater.inflate(R.layout.fragment_dialogo,null);
+        builder.setView(v);
+
+        txtEstatus = (TextView) v.findViewById(R.id.txtEstatus);
+        txtMensaje = (TextView) v.findViewById(R.id.txtMensajeDialogo);
+        btnAceptar = (TextView) v.findViewById(R.id.btnAceptarDialog);
+
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if(context instanceof Activity){
+            this.actividad  = (Activity) context;
+        }else{
+            throw new RuntimeException(context.toString());
+        }
+    }
+}
--- app/src/main/java/com/example/superandome_appfinal/Vistas/Consultante/cargarEmocionDiaria.java
+++ app/src/main/java/com/example/superandome_appfinal/Vistas/Consultante/cargarEmocionDiaria.java
@@ -2,19 +2,41 @@ package com.example.superandome_appfinal.Vistas.Consultante;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.superandome_appfinal.Dialogos.dialogo;
 import com.example.superandome_appfinal.R;
 
 public class cargarEmocionDiaria extends Fragment {
+
+    Button btnRegistrar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_cargar_emocion_diaria, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        btnRegistrar = (Button) view.findViewById(R.id.btnRegistrar);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogo d = new dialogo();
+                d.show(getActivity().getSupportFragmentManager(), "fragment_dialogo");
+            }
+        });
     }
 }
