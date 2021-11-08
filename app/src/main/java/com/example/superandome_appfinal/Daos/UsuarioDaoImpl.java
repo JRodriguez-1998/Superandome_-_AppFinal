c7478e627ee4db1e2395be6cdc9b887521153570
JRodriguez-1998 <jose.rodriguez@alumnos.frgp.utn.edu.ar>
2021-11-08 17:17:35 -0300

    Conexion BD y LOGIN (A arreglar)

--- app/src/main/java/com/example/superandome_appfinal/Daos/UsuarioDaoImpl.java
+++ app/src/main/java/com/example/superandome_appfinal/Daos/UsuarioDaoImpl.java
@@ -1,14 +1,57 @@
 package com.example.superandome_appfinal.Daos;
 
+import com.example.superandome_appfinal.Entidades.Consejo;
 import com.example.superandome_appfinal.Entidades.Usuario;
 import com.example.superandome_appfinal.Helpers.DataDB;
 import com.example.superandome_appfinal.IDaos.UsuarioDao;
 import com.j256.ormlite.dao.BaseDaoImpl;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class UsuarioDaoImpl extends BaseDaoImpl<Usuario, Integer> implements UsuarioDao {
     public UsuarioDaoImpl() throws SQLException {
         super(DataDB.getConnectionSource(), Usuario.class);
     }
+
+    @Override
+    public Usuario obtenerUsuario(String nick, String pass) throws android.database.SQLException {
+
+        Map<String, Object> filtros = new HashMap<>();
+        filtros.put("nickname", nick);
+        filtros.put("password", pass);
+
+        try {
+            return (Usuario) queryForFieldValues(filtros);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    public List<Usuario> findUser(String nick, String pass) throws SQLException
+    {
+        Map<String, Object> userMap = new HashMap<>();
+        userMap.put("nickname", nick);
+        userMap.put("password", pass);
+        List<Usuario> userlistEntities= queryForFieldValues(userMap);
+        return userlistEntities==null?null:userlistEntities;
+    }
+
+    @Override
+    public List<Usuario> getUsers(String nick, String pass) throws SQLException {
+
+        Map<String, Object> filtros = new HashMap<>();
+        filtros.put("nickname", nick);
+        filtros.put("password", pass);
+
+        return queryForFieldValues(filtros);
+
+        //return this.queryForEq("nickaname", nick);
+    }
+
 }
--- app/src/main/java/com/example/superandome_appfinal/IDaos/UsuarioDao.java
+++ app/src/main/java/com/example/superandome_appfinal/IDaos/UsuarioDao.java
@@ -1,7 +1,16 @@
 package com.example.superandome_appfinal.IDaos;
 
+import android.database.SQLException;
+
 import com.example.superandome_appfinal.Entidades.Usuario;
 import com.j256.ormlite.dao.Dao;
 
+import java.util.List;
+
 public interface UsuarioDao extends Dao<Usuario, Integer> {
+    Usuario obtenerUsuario(String nick, String pass) throws SQLException;
+
+    List<Usuario> getUsers(String nick, String pass) throws java.sql.SQLException;
+
+
 }
