eb7cfc0f5885ebe987be2422e388823d0bff61e1
Marcos Vera <73365993+marcosvera2@users.noreply.github.com>
2021-11-10 20:30:34 -0300

    Carga lista rutina
    Carga lista rutina

--- app/src/main/java/com/example/superandome_appfinal/IDaos/ItemDao.java
+++ app/src/main/java/com/example/superandome_appfinal/IDaos/ItemDao.java
@@ -3,5 +3,10 @@ package com.example.superandome_appfinal.IDaos;
 import com.example.superandome_appfinal.Entidades.Item;
 import com.j256.ormlite.dao.Dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ItemDao extends Dao<Item, Integer> {
+
+    List<Item> getItemsRutina() throws SQLException;
 }
