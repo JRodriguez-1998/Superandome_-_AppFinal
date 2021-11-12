e51cb147d62d53313b682a6f37069b35b7eb1f6a
mauricio-molina-git <81722575+mauricio-molina-git@users.noreply.github.com>
2021-11-12 00:50:34 -0300

    Recuperar contraseña: consultante
    Se creo un dialogo para ingresar la contraseña nueva. se realizaron todas las validaciones correspondieentes(respuesta correcta, pregunta elegida, campos vacios)

--- app/src/main/java/com/example/superandome_appfinal/Dialogos/dialogoSugerirConsejo.java
+++ app/src/main/java/com/example/superandome_appfinal/Dialogos/dialogoSugerirConsejo.java
@@ -8,11 +8,9 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.superandome_appfinal.R;
@@ -38,7 +36,7 @@ public class dialogoSugerirConsejo extends DialogFragment {
 
         txtEstatus = (TextView) v.findViewById(R.id.txtEstatus);
         txtMensaje = (TextView) v.findViewById(R.id.txtMensajeDialogo);
-        btnAceptar = (TextView) v.findViewById(R.id.btnAceptarDialog);
+        btnAceptar = (TextView) v.findViewById(R.id.btnAceptarDialogCambioPass);
 
         eventoBotones();
 
