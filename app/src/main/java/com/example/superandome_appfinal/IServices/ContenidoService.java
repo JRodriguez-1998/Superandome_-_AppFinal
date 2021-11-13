216c1a26091cf971adaf17d84b3221eb190b9c2d
JRodriguez-1998 <jose.rodriguez@alumnos.frgp.utn.edu.ar>
2021-11-13 08:53:31 -0300

    Lista Multimedia

--- app/src/main/java/com/example/superandome_appfinal/IServices/ContenidoService.java
+++ app/src/main/java/com/example/superandome_appfinal/IServices/ContenidoService.java
@@ -2,7 +2,10 @@ package com.example.superandome_appfinal.IServices;
 
 import com.example.superandome_appfinal.Entidades.Contenido;
 
+import java.util.List;
+
 public interface ContenidoService {
     Boolean guardar(Contenido contenido);
     Contenido getContenidoByID(int id);
+    List<Contenido> getContenidosAprobados();
 }
--- app/src/main/java/com/example/superandome_appfinal/Vistas/Consultante/multimedia.java
+++ app/src/main/java/com/example/superandome_appfinal/Vistas/Consultante/multimedia.java
@@ -2,65 +2,72 @@ package com.example.superandome_appfinal.Vistas.Consultante;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.superandome_appfinal.Dialogos.dialogoAprobarConsejo;
+import com.example.superandome_appfinal.Entidades.Consejo;
+import com.example.superandome_appfinal.Entidades.Contenido;
+import com.example.superandome_appfinal.IServices.ContenidoService;
 import com.example.superandome_appfinal.R;
+import com.example.superandome_appfinal.Services.ConsejoServiceImpl;
+import com.example.superandome_appfinal.Services.ContenidoServiceImpl;
+import com.example.superandome_appfinal.Vistas.Director.adapterDirector;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link multimedia#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class multimedia extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    RecyclerView recyclerViewContenido;
+    ArrayList<Contenido> contenidos;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public multimedia() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment multimedia.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static multimedia newInstance(String param1, String param2) {
-        multimedia fragment = new multimedia();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    ContenidoService contenidoService;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_multimedia, container, false);
+        View view = inflater.inflate(R.layout.fragment_multimedia, container, false);
+
+        try {
+            contenidoService = new ContenidoServiceImpl();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        contenidos = new ArrayList<Contenido>();
+        recyclerViewContenido=(RecyclerView) view.findViewById(R.id.rvContenido);
+        recyclerViewContenido.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerViewContenido.setHasFixedSize(true);
+
+        obtenerContenido();
+
+        adapterMultimedia adapterContenido = new adapterMultimedia(contenidos);
+
+        adapterContenido.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //Capturar IdContenido y enviarlo a su fragment
+                int idContenido = (int) contenidos.get(recyclerViewContenido.getChildAdapterPosition(view)).getIdContenido();
+                Toast.makeText(getActivity(),"IdConsejo: " + idContenido, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        recyclerViewContenido.setAdapter(adapterContenido);
+
+        return view;
+    }
+
+    public void obtenerContenido(){
+
+        for(int i = 0; i< contenidoService.getContenidosAprobados().size(); i ++){
+            contenidos.add(contenidoService.getContenidosAprobados().get(i));
+        }
     }
 }
