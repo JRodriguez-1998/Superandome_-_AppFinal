aafeb9f6d6ad30cf31b89f1c5729f40eb74ca2ae
mauricio-molina-git <81722575+mauricio-molina-git@users.noreply.github.com>
2021-11-15 14:57:33 -0300

    Enum tipo de archivo
    Se agrego enum de tipo de archivo para no utilizar 1,2,3

--- app/src/main/java/com/example/superandome_appfinal/Constantes/TipoArchivoEnum.java
+++ app/src/main/java/com/example/superandome_appfinal/Constantes/TipoArchivoEnum.java
@@ -1,4 +1,26 @@
 package com.example.superandome_appfinal.Constantes;
 
 public enum TipoArchivoEnum {
+    PDF(1),
+    VIDEO(2),
+    AUDIO(3);
+
+    private final int tipo;
+
+    TipoArchivoEnum(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public static TipoArchivoEnum getTipoArchivo(int tipo) {
+        for (TipoArchivoEnum t : TipoArchivoEnum.values()) {
+            if (t.getTipo() == tipo) {
+                return t;
+            }
+        }
+        return null;
+    }
 }
