298d07e69aa89a418d8fe7b6c43600729ac2a8eb
JRodriguez-1998 <jose.rodriguez@alumnos.frgp.utn.edu.ar>
2021-11-15 15:22:19 -0300

    Opcion VER Profesional

---
+++ app/src/main/java/com/example/superandome_appfinal/Vistas/Profesional/multimedia_text_profesional.java
@@ -0,0 +1,99 @@
+package com.example.superandome_appfinal.Vistas.Profesional;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+
+import com.example.superandome_appfinal.Entidades.Contenido;
+import com.example.superandome_appfinal.IServices.ContenidoService;
+import com.example.superandome_appfinal.R;
+import com.example.superandome_appfinal.Services.ContenidoServiceImpl;
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.util.Base64;
+
+public class multimedia_text_profesional extends Fragment {
+    PDFView pdf;
+    TextView txt;
+    Integer idContenido;
+
+    public multimedia_text_profesional(){};
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        try {
+
+
+            SharedPreferences preferences = requireActivity().getSharedPreferences("contenido", Context.MODE_PRIVATE);
+            idContenido = preferences.getInt("idContenido", 0);
+
+
+//        String path = Environment.getExternalStorageDirectory()+"/Download/HOLA.pdf";
+//        // String filename = "HOLA.pdf";
+//
+//        File file = new File(path);
+//
+////        Path ruta = file.toPath();
+//
+//
+//        //FUNCA
+////        File file = new File(requireContext().getExternalFilesDir(null),"HOLA.pdf");
+//        byte[] bytesDato= new byte[0];
+//        try {
+//
+//
+//            bytesDato = Files.readAllBytes(file.toPath());
+//
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        String resultado = Base64.getEncoder().encodeToString(bytesDato);
+//
+//
+//        txt = (TextView) view.findViewById(R.id.txtPrueba);
+//        txt.setText(resultado);
+            ContenidoService contenidoService = null;
+
+            contenidoService = new ContenidoServiceImpl();
+
+
+
+
+
+            Contenido contenido = contenidoService.getContenidoByID(idContenido);
+
+            byte[] decoder = Base64.getDecoder().decode(contenido.getArchivo());
+            //byte[] decodedString
+
+
+
+            pdf = (PDFView) view.findViewById(R.id.pdfView);
+            pdf.fromBytes(decoder).load();
+        }catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getActivity(), "Ha ocurrido un error al inicializar la pantalla", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_multimedia_text, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/superandome_appfinal/Vistas/Profesional/multimedia_video_profesional.java
@@ -0,0 +1,116 @@
+package com.example.superandome_appfinal.Vistas.Profesional;
+
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.superandome_appfinal.Entidades.Contenido;
+import com.example.superandome_appfinal.IServices.ContenidoService;
+import com.example.superandome_appfinal.R;
+import com.example.superandome_appfinal.Services.ContenidoServiceImpl;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public class multimedia_video_profesional extends Fragment {
+    VideoView videoView;
+    MediaController mediaController;
+    InputStream inputStream;
+
+    public multimedia_video_profesional() {
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        try {
+            videoView = view.findViewById(R.id.videoView2);
+
+            ContenidoService contenidoService = new ContenidoServiceImpl();
+            Contenido contenido = contenidoService.getContenidoByID(3);
+
+            byte[] decoder = Base64.decode(contenido.getArchivo(), Base64.DEFAULT);
+            inputStream = new ByteArrayInputStream(decoder);
+
+            mediaController = new MediaController(getContext());
+
+            mediaController.setMediaPlayer(videoView);
+            videoView.setMediaController(mediaController);
+            videoView.requestFocus();
+
+            run();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public void run() {
+        try {
+            File temp = File.createTempFile("test", "mp4");
+            temp.deleteOnExit();
+
+            File bufferFile = File.createTempFile("test", "mp4");
+            BufferedOutputStream bufferOS = new BufferedOutputStream(new FileOutputStream(bufferFile));
+
+
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream, 2048);
+
+            byte[] buffer = new byte[16384];
+            long totalRead = 0;
+            boolean started = false;
+            int numRead = bufferedInputStream.read(buffer);
+            while (numRead != -1) {
+                bufferOS.write(buffer, 0, numRead);
+                bufferOS.flush();
+
+                totalRead += numRead;
+                if (totalRead > 120000 && !started) {
+                    setSourceAndStartPlay(bufferFile);
+                    started = true;
+                }
+
+                numRead = bufferedInputStream.read(buffer);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void setSourceAndStartPlay(File bufferFile) {
+        try {
+//            videoView.getCurrentPosition();
+            videoView.setVideoPath(bufferFile.getAbsolutePath());
+
+//            videoView.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_multimedia_video, container, false);
+    }
+}
