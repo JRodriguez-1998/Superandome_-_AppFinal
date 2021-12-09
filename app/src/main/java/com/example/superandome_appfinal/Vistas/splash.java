380f055ed22c997c2e882d750d8bbcdb74e94b4e
JRodriguez-1998 <88742239+JRodriguez-1998@users.noreply.github.com>
2021-12-09 13:15:58 -0300

    Update splash.java

--- app/src/main/java/com/example/superandome_appfinal/Vistas/splash.java
+++ app/src/main/java/com/example/superandome_appfinal/Vistas/splash.java
@@ -8,6 +8,10 @@ import com.example.superandome_appfinal.R;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class splash extends AppCompatActivity {
 
@@ -15,9 +19,20 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        //Animaciones para Splash
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        ImageView logoSuperandome = findViewById(R.id.imageView2);
+        ImageView logoSoyUz = findViewById(R.id.imageView5);
+
+        logoSuperandome.setAnimation(animacion1);
+        logoSoyUz.setAnimation(animacion2);
 
         new Handler().postDelayed(new Runnable() {
             @Override
